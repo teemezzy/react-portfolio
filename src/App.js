54a08073e207a9360c2f3d35b64749c48c46ca8c
David ThankGod <toromothankgod@gmail.com>
2021-12-12 16:13:55 -0600

    trying third commit

--- src/App.js
+++ src/App.js
@@ -2,17 +2,14 @@ import Navbar from './components/Navbar'
 import './App.css'
 import Main from './components/Main'
 import Footer from './components/Footer'
-import { Switch, Route } from 'react-router-dom'
 
 function App() {
   return (
     <div className='App'>
       <Navbar />
-      <Switch>
-        <Route exact path='/'>
+      
           <Main />
-        </Route>
-      </Switch>
+        
       <Footer />
     </div>
   )
